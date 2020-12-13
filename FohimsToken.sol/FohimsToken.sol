6bb75cf76912da21db9cbd2034dce7fed6b39a7e
Fohim Neusurg <75149703+FohimNeusburg@users.noreply.github.com>
2020-12-13 16:06:07 +0000

    Add files via upload

---
+++ FohimsToken.sol/FohimsToken.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.4;
+
+import"./SafeMath.sol";
+import"./GSNContext.sol";
+import"./IERC20.sol";
+
+contract ERC20 is IERC20, GSNContext {
+    
+    using SafeMath for uint256;
+    
+    uint private _decimals;
+    uint private _totalsupply;
+    
+    string private _name;
+    string private _ticker;
+    
+    address public _owner;
+    
+    mapping (address => uint) private _balances;
+    mapping (address => mapping (address => uint)) private _allowances;
+
+
+    constructor () {
+        _decimals = 10;
+        _totalsupply = 100 * (10 ** _decimals);
+        _name = "FohimNuesburgsTokenContract";
+        _ticker = "FNT";
+        _owner = msg.sender;
+        _balances[_owner] = _balances[_owner].add(_totalsupply);
+    }
+    
+    function name() public view returns (string memory) {
+        return _name;
+    }
+    
+    function ticker() public view returns (string memory) {
+        return _ticker;
+    }
+    
+    function totalSupply() public view override returns (uint) {
+        return _totalsupply;
+    }
+    
+    function getBalance(address _key) public view override returns (uint) {
+        return _balances[_key];
+    }
+    
+    function allowance(address _from, address _spender) public view override returns (uint) {
+        return _allowances[_from][_spender];
+    }
+    
+    function approve(address _spender, uint _amount) public override returns (bool) {
+        require (_balances[msg.sender] >= _amount);
+        _allowances[msg.sender][_spender] = _allowances[msg.sender][_spender].add(_amount);
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+    
+    function transfer(address _too, uint _amount) public override returns (bool) {
+        require (_balances[msg.sender] >=_amount);
+        require (msg.sender != address(0), "ERC20: Is not the zero address");
+        require (_too != address(0), "ERC20: Is not the zero address");
+        
+        _balances[msg.sender] = _balances[msg.sender].sub(_amount);
+        _balances[_too] = _balances[_too].add(_amount);
+        
+        emit Transfer(msg.sender, _too, _amount);
+        
+        return true;
+    }
+    
+    function transferFrom(address _from, address _spender, uint _amount) public override returns (bool) {
+        require (_allowances[_from][_spender] >= _amount);
+        require (_balances[_from] >= _amount);
+        
+        transfer(_spender, _amount);
+        
+        _allowances[_from][_spender] = _allowances[_from][_spender].sub(_amount);
+        
+        return true;
+    }
+    
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
+}
